9c851ea91daf07fbc9429f7a5dc001736564f466
GamerboyTR <offical.gamerboytr@yandex.com>
2021-07-15 23:33:22 +0300

    Darkmode Düzeltmeleri

--- background.js
+++ background.js
@@ -5,6 +5,6 @@ $(function () {
 	if (window.location.href.match(/setting/g)) {
 	}
 	if (darkmode) {
-		$("head").append('<link rel="stylesheet" href="src/darkmode.css">');
+		$("head").append('<link rel="stylesheet" href="https://raw.githubusercontent.com/gamerboytr/Tiktok-Plus-Plus/main/src/css/darkmode.css">');
 	}
 });
