f7418be27e874a224bfa39ee815f135f67f9adab
GamerboyTR <59420445+gamerboytr@users.noreply.github.com>
2021-07-18 18:03:19 +0300

    Adminler eklendi

---
+++ src/js/admins.js
@@ -0,0 +1,14 @@
+const Admins = [
+	{
+		name: "GamerboyTR",
+		perm_level: "owner",
+		username: "_nebakyonyusufiste_",
+		perm_name: "Tiktok++ Kurucusu",
+	},
+	{
+		name: "Mabel",
+		perm_level: "dearmember",
+		username: "nebakyonmabeliste",
+		perm_name: "Değerli Üye",
+	},
+];
